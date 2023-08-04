2954bbf3f66a1ac62ca4d483b2551be062e83f58
s1f102101615 <s1f102101615@iniad.org>
2023-08-04 17:37:49 +0900

    テスト

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>約束アプリの作成</Text>
+      <Text>約束アプの作成</Text>
       <StatusBar style="auto" />
     </View>
   );
