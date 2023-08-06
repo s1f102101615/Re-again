5235e2f23c67bcdae6694eae34e4480d8b2ad7a8
s1f102101615 <s1f102101615@iniad.org>
2023-08-06 18:04:34 +0900

    ログアウトボタン実装

--- page/ProfileScreen.js
+++ page/ProfileScreen.js
@@ -1,13 +1,39 @@
 // ProfileScreen.js
 
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { auth, onAuthStateChanged, signOut } from '../firebase';
+import { useNavigation } from '@react-navigation/native';
 
 const ProfileScreen = () => {
+  const navigation = useNavigation();
+  
+  const handleLogout = () => {
+    auth.signOut(auth)
+      .then(() => {
+        console.log('logout');
+        navigation.navigate('Login');
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
   return (
-    <View style={styles.container}>
-      <Text>Profile Screen</Text>
-    </View>
+    <View>
+    <Text>ホーム画面</Text>
+    <TouchableOpacity
+      onPress={handleLogout}
+      style={{
+        marginTop: 10,
+        padding: 10,
+        backgroundColor: '#88cb7f',
+        borderRadius: 10,
+        width: 100,
+      }}
+    >
+      <Text style={{ color: 'white' }}>ログアウト</Text>
+    </TouchableOpacity>
+  </View>
   );
 };
 
