7652a6a563c4b19a56c9447fd44a6ab81e3dbc93
s1f102101615 <s1f102101615@iniad.org>
2023-08-18 21:45:34 +0900

    トークメッセージの処理

--- page/TalkRoom.tsx
+++ page/TalkRoom.tsx
@@ -2,11 +2,11 @@ import React, { useEffect, useState, useRef } from 'react';
 import { View, Text, StyleSheet, ScrollView, TouchableOpacity, TextInput, KeyboardAvoidingView, Platform } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { auth, firestore } from '../firebase';
-import { collection, doc, onSnapshot, query, setDoc } from 'firebase/firestore';
+import { collection, doc, onSnapshot, getDoc, setDoc, updateDoc, arrayUnion, query, where, orderBy, getDocs } from 'firebase/firestore';
 import { useNavigation, useRoute } from '@react-navigation/native';
 
 const TalkRoom = () => {
-  const [messages, setMessages] = useState<{ id: string; text: string }[]>([]);
+  const [messages, setMessages] = useState<{ id: string; text: string; name: string; uid: string; createdAt: number }[]>([]);
   const [inputText, setInputText] = useState('');
   const scrollViewRef = useRef<ScrollView>(null);
 
@@ -14,16 +14,51 @@ const TalkRoom = () => {
   const route = useRoute();
   const { talkroomId } = route.params as { talkroomId: string };
 
-  const handleSend = () => {
-    // メッセージを送信する処理
-    setMessages([...messages, { id: String(messages.length + 1), text: inputText }]);
+  const handleSend = async () => {
+    const user = auth.currentUser;
+    if (!user) {
+      return;
+    }
+    const { uid, displayName } = user;
+    const createdAt = Date.now();
+    const message = { id: String(messages.length + 1), text: inputText, name: displayName, uid, createdAt };
+    setMessages([...messages, message]);
     setInputText('');
+    const talkroomRef = doc(firestore, 'talkroom', talkroomId);
+    // docがあるならupdateDoc、ないならsetDoc
+    const talkroomDoc = await getDoc(doc(collection(firestore, 'talkroom'), talkroomId));
+    console.log(talkroomDoc.exists());
+    if (talkroomDoc.exists()) {
+        console.log('Document data:', talkroomDoc.data());
+        await updateDoc(talkroomRef, { messages: [...talkroomDoc.data().messages, message] });
+      } else {
+        console.log('No such document!');
+        await setDoc(talkroomRef, { messages: [message] });
+      }
   };
-  const renderItem = ({ item }) => (
-    <View style={styles.message}>
-      <Text style={styles.messageText}>{item.text}</Text>
-    </View>
-  );
+  
+  useEffect(() => {
+    const fetchMessages = async () => {
+      const user = auth.currentUser;
+      if (!user) {
+        return;
+      }
+      const { uid } = user;
+      const messageRef = doc(firestore, 'talkroom', talkroomId);
+      const messageSnapshot = await getDoc(messageRef);
+      const data = messageSnapshot.data();
+      const messages = [];
+      if (data && data.messages) {
+        data.messages.forEach((message) => {
+            messages.push({ ...message });
+        });
+      }
+      setMessages(messages);
+    };
+    fetchMessages();
+  }, [talkroomId]);
+
+
   useEffect(() => {
     if (scrollViewRef.current) {
       scrollViewRef.current.scrollToEnd({ animated: true });
@@ -39,9 +74,16 @@ const TalkRoom = () => {
       <ScrollView ref={scrollViewRef} contentContainerStyle={styles.contentContainer}>
         <Text style={styles.header}>Talk Room {talkroomId}</Text>
         {messages.map((message) => (
-          <View key={message.id} style={styles.message}>
-            <Text style={styles.messageText}>{message.text}</Text>
-          </View>
+          <View
+          key={message.id}
+          style={[
+            styles.message,
+            message.uid === auth.currentUser?.uid ? styles.rightMessage : styles.leftMessage,
+          ]}
+        >
+          <Text style={styles.messageText}>{message.text}</Text>
+          <Text style={styles.messageInfo}>{message.name} ({message.uid})</Text>
+        </View>
         ))}
       </ScrollView>
       <View style={styles.inputContainer}>
@@ -76,14 +118,18 @@ const styles = StyleSheet.create({
     marginVertical: 4,
     marginHorizontal: 8,
     maxWidth: '80%',
+    
   },
   messageText: {
     fontSize: 16,
   },
+  messageInfo: {
+    fontSize: 12,
+    color: '#888',
+  },
   inputContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'flex-end',
     backgroundColor: '#eee',
     padding: 8,
     borderTopWidth: 1,
@@ -103,6 +149,23 @@ const styles = StyleSheet.create({
   },
   contentContainer: {
     flexGrow: 1,
+    justifyContent: 'flex-end',
+  },
+  leftMessage: {
+    alignSelf: 'flex-start',
+    backgroundColor: '#eee',
+    borderRadius: 8,
+    marginVertical: 4,
+    marginHorizontal: 8,
+    maxWidth: '80%',
+  },
+  rightMessage: {
+    alignSelf: 'flex-end',
+    backgroundColor: 'lightblue', // 右寄せメッセージの背景色
+    borderRadius: 8,
+    marginVertical: 4,
+    marginHorizontal: 8,
+    maxWidth: '80%',
   },
 });
 
