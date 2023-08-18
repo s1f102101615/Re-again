181b780b58730ad998b49042f4f49dceda8c51ef
s1f102101615 <s1f102101615@iniad.org>
2023-08-18 23:54:49 +0900

    friend人数

--- page/HomeScreen.tsx
+++ page/HomeScreen.tsx
@@ -2,23 +2,50 @@
 
 import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import { auth } from '../firebase';
+import { auth, firebase, firestore } from '../firebase';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import { User } from 'firebase/auth';
+import { DocumentData, DocumentReference, collection, doc, getDoc } from 'firebase/firestore';
 
 const HomeScreen = () => {
-  const [user, setUser] = useState('');
+  const [user, setUser] = useState<User>();
+  const [displayName, setDisplayName] = useState('');
+  const [friendsCount, setFriendsCount] = useState(0);
+
+  const db = firestore;
+
+  const getFriendsCount = async (uid: string): Promise<number> => {
+    const friendRef: DocumentReference<DocumentData> = doc(db, `users/${uid}/friends`);
+    const friendSnapshot = await getDoc(friendRef);
+    const friendData = friendSnapshot.data();
+    if (friendData) {
+      return Object.keys(friendData).length;
+    } else {
+      return 0;
+    }
+  };
+
+
   // ログイン状態の監視
   useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((user) => {
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
       if (user) {
-        setUser(user.displayName || '');
+        setUser(user);
+        const count = await getFriendsCount(user.uid);
+      setFriendsCount(count);
       } else {
-        setUser('');
+        setUser(undefined);
       }
     });
     return () => unsubscribe();
   }, []);
-  const displayName = user || '未定義';
+
+  useEffect(() => {
+    if (user) {
+      setDisplayName(user.displayName);
+    }
+  }, []);
+
 
     // プロフィールアイコンを変更する処理
   // const handleChoosePhoto = () => {
@@ -36,24 +63,56 @@ const HomeScreen = () => {
 
   return (
     <View style={styles.container}>
-      <Text>Home Screen</Text>
-      <View>
-      <View style={{ alignItems: 'center', marginTop:'10%', marginBottom:'6%' }}>
+    <View>
+      <View style={{ alignItems: 'center', marginTop: '10%', marginBottom: '6%' }}>
         <Icon name="user" size={80} color="black" />
       </View>
       <View style={{ alignItems: 'center' }}>
         <Text style={{ fontSize: 20 }}>{displayName}</Text>
-      </View>
+        <Text style={{ fontSize: 16 }}>友達の数: {friendsCount}</Text>
       </View>
     </View>
+  </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#fff',
+  },
+  header: {
+    height: 60,
+    backgroundColor: '#2196F3',
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  headerText: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  content: {
+    flex: 1,
+    padding: 20,
+  },
+  profile: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  iconContainer: {
+    marginRight: 20,
+  },
+  profileInfo: {
+    flex: 1,
+  },
+  displayName: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  friendsCount: {
+    fontSize: 16,
   },
 });
 
