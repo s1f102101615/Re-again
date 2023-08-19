eab29b6a0587af81a839b3fff45b0f96f680008d
s1f102101615 <s1f102101615@iniad.org>
2023-08-20 00:33:44 +0900

    apoiner作成

---
+++ page/apoiner.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+
+function apointer(props) {
+
+  return (
+    <>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+   
+});
+
+
+export default apointer;
