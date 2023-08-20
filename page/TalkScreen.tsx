698e1bb579b8027fbf076662c9803eb4b3a51000
s1f102101615 <s1f102101615@iniad.org>
2023-08-20 16:53:41 +0900

    約束からトークに行けるように * 戻ったらトークに戻るようにしたい

--- page/TalkScreen.tsx
+++ page/TalkScreen.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
-import TalkRoom from './TalkRoom';
+import TalkRoom from './TalkScreen/TalkRoom';
 import { createStackNavigator } from '@react-navigation/stack';
-import TalkList from './TalkList';
+import TalkList from './TalkScreen/TalkList';
 
 const Stack = createStackNavigator();
 
